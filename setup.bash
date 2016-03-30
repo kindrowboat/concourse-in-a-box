196d344cbae780f302929efac2998da7d52366a2
Tom Dunlap <stevetom@motevets.com>
2016-03-30 19:29:20 +0000

    Everything is better with emoji.

--- setup.bash
+++ setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "🛫 Let's get you flying with concourse.\n"
+
 set -eux
 
 sudo apt-get update
@@ -17,11 +19,11 @@ local   all             all                                     peer
 host    all             all             127.0.0.1/32            trust
 host    all             all             ::1/128                 trust
 EOF
-sudo service postgresql restart
 
 wget https://github.com/concourse/concourse/releases/download/v1.0.0/concourse_linux_amd64
 sudo install --owner=root --group=root --mode=744 concourse_linux_amd64 /usr/local/sbin/concourse
 sudo install --owner=root --group=root --mode=744 ciab /usr/local/sbin/ciab
 
 set +x
-echo "Ready to go! Run 'sudo ciab'."
+
+echo "\n🛬 Ready to go! Reboot and run 'sudo ciab' to start concourse."
