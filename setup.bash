2cae4d684a872b0b40beef2845ce881daa3ff1f3
Tom Dunlap <stevetom@motevets.com>
2016-04-08 02:54:46 +0000

    Detect and use PostgreSQL unix sockets
    - do not require ciab to be run as root
    - do not create un-need db user
    - do not download binary again if already downloaded

--- setup.bash
+++ setup.bash
@@ -5,26 +5,19 @@ printf "🛫 Let's get you flying with concourse.\n\n"
 set -eux
 cd "$(dirname "$0")"
 
-sudo apt-get update
 sudo apt-get -y install postgresql postgresql-contrib linux-generic-lts-vivid
 sudo update-rc.d postgresql enable
 set +e
 sudo -u postgres createuser -d --superuser $(whoami)
-createuser --superuser root
 createdb atc
 set -e
-cat <<EOF | sudo tee /etc/postgresql/9.3/main/pg_hba.conf
-# TYPE  DATABASE        USER            ADDRESS                 METHOD
-local   all             postgres                                peer
-local   all             all                                     peer
-host    all             all             127.0.0.1/32            trust
-host    all             all             ::1/128                 trust
-EOF
 
-wget https://github.com/concourse/concourse/releases/download/v1.0.0/concourse_linux_amd64
-sudo install --owner=root --group=root --mode=744 concourse_linux_amd64 /usr/local/sbin/concourse
-sudo install --owner=root --group=root --mode=744 ciab /usr/local/sbin/ciab
+if [[ ! -f concourse_linux_amd64 ]]; then
+  wget https://github.com/concourse/concourse/releases/download/v1.0.0/concourse_linux_amd64
+fi
+sudo install --owner=concourse --group=concourse --mode=755 concourse_linux_amd64 /usr/local/bin/concourse
+sudo install --owner=concourse --group=concourse --mode=755 ciab /usr/local/bin/ciab
 
 set +x
 
-printf "\n🛩 Ready to go! Reboot and run 'sudo ciab' to start concourse.\n\n"
+printf "\n🛩 Ready to go! Run reboot to reload the kernel, login as concourse, and run 'ciab' to start concourse.\n\n"
